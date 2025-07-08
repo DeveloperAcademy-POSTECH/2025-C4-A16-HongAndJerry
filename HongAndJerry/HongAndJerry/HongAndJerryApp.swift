53dbeb86a307be382ef48a6b1a718f64b7db044c
Rama <gudrms2538@gmail.com>
2025-07-08 15:18:13 +0900

    add: create project

---
+++ HongAndJerry/HongAndJerry/HongAndJerryApp.swift
@@ -0,0 +1,17 @@
+//
+//  HongAndJerryApp.swift
+//  HongAndJerry
+//
+//  Created by Rama on 7/8/25.
+//
+
+import SwiftUI
+
+@main
+struct HongAndJerryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
