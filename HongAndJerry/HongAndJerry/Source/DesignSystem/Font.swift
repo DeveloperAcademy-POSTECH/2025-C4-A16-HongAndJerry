a1d27f3a05c93e65b1f41eb419b1435cee1b6085
Hong <dbsghdz1@gmail.com>
2025-07-17 00:54:27 +0900

    design: 폰트 추가

---
+++ HongAndJerry/HongAndJerry/Source/DesignSystem/Font.swift
@@ -0,0 +1,16 @@
+//
+//  Font.swift
+//  HongAndJerry
+//
+//  Created by Hong on 7/17/25.
+//
+
+import SwiftUI
+
+extension Font {
+    static let SUITTitle  = Font.custom("SUIT-SemiBold", size: 24)
+    static let SUITBody   = Font.custom("SUIT-SemiBold", size: 12)
+    static let SUITHeader = Font.custom("SUIT-SemiBold", size: 16)
+    static let SUITTimer  = Font.custom("SUIT-Regular", size: 12)
+}
+
