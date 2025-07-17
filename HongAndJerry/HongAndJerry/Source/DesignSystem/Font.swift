cf251b786a0f5d6dd4f0b9f2acbdd991757a3056
Hong <dbsghdz1@gmail.com>
2025-07-17 17:41:43 +0900

    design: font, spacing description

--- HongAndJerry/HongAndJerry/Source/DesignSystem/Font.swift
+++ HongAndJerry/HongAndJerry/Source/DesignSystem/Font.swift
@@ -8,9 +8,13 @@
 import SwiftUI
 
 extension Font {
+    ///"SUIT-SemiBold", size: 24
     static let SUITTitle  = Font.custom("SUIT-SemiBold", size: 24)
+    ///"SUIT-SemiBold", size: 12
     static let SUITBody   = Font.custom("SUIT-SemiBold", size: 12)
+    ///"SUIT-SemiBold", size: 16
     static let SUITHeader = Font.custom("SUIT-SemiBold", size: 16)
+    ///"SUIT-Regular", size: 12
     static let SUITTimer  = Font.custom("SUIT-Regular", size: 12)
 }
 
