4956e2f4112d1217c386774d91eac0b486801c7a
Hong <dbsghdz1@gmail.com>
2025-07-19 20:13:50 +0900

    feat: 알럿 모델생성
    #7

---
+++ HongAndJerry/HongAndJerry/Source/Presentation/Export/Model/ExportAlertModel.swift
@@ -0,0 +1,12 @@
+//
+//  AlertModel.swift
+//  HongAndJerry
+//
+//  Created by Hong on 7/19/25.
+//
+
+struct ExportAlertModel {
+    let title: String
+    let message: String
+    let buttonTitle: String
+}
