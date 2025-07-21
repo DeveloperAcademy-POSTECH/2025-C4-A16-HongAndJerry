7e99b1f9eb79d0d8146e2c1144456d7a4b8f2950
Hong <dbsghdz1@gmail.com>
2025-07-21 16:01:42 +0900

    feat: NavigationRouter 추가

---
+++ HongAndJerry/HongAndJerry/Source/Service/Navigation/NavigationRouter.swift
@@ -0,0 +1,49 @@
+//
+//  NavigationRouter.swift
+//  HongAndJerry
+//
+//  Created by Hong on 7/20/25.
+//
+
+import Foundation
+
+public enum Screen: Hashable {
+    case home
+    case selectFrame
+    case selectVideo
+    case editVideoRatio
+    case videoEditView
+}
+
+@Observable
+final class Router: ObservableObject {
+    public var route: [Screen] = []
+    public init() { }
+    
+    @MainActor
+    public func push(screen: Screen) {
+        route.append(screen)
+    }
+    
+    @MainActor
+    public func pop() {
+        route.removeLast()
+    }
+    
+    @MainActor
+    public func pop(depth: Int) {
+        route.removeLast(depth)
+    }
+    
+    @MainActor
+    public func popToRoot() {
+        route.removeAll()
+    }
+    
+    @MainActor
+    public func switchScreen(screen: Screen) {
+        guard !route.isEmpty else { return }
+        let lastIndex = route.count - 1
+        route[lastIndex] = screen
+    }
+}
