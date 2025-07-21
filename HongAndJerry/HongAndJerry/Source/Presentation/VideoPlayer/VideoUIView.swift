d5ff82e0c1152453a667d56b96324793f35ea769
ehdrbdndns <ehdrbdndns@naver.com>
2025-07-21 12:41:07 +0900

    feat: VIdeoPlayerView Layout 구성

---
+++ HongAndJerry/HongAndJerry/Source/Presentation/VideoPlayer/VideoUIView.swift
@@ -0,0 +1,45 @@
+
+//
+//  VideoPlayerUIView.swift
+//  HongAndJerry
+//
+//  Created by Donggyun Yang on 7/12/25.
+//
+
+import UIKit
+import AVFoundation
+
+class VideoUIView: UIView {
+    
+    // MARK: - Properties
+    
+    private var playerLayer: AVPlayerLayer {
+        self.layer as! AVPlayerLayer
+    }
+    
+    var player: AVPlayer? {
+        get {
+            playerLayer.player
+        }
+        set {
+            playerLayer.player = newValue
+        }
+    }
+    
+    // MARK: - Lifecycle
+    
+    override class var layerClass: AnyClass {
+        AVPlayerLayer.self
+    }
+    
+    // MARK: - Init
+    
+    init(player: AVPlayer) {
+        super.init(frame: .zero)
+        self.player = player
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
