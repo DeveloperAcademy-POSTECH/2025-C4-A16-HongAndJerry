7f904ab116523058934764fc1f5cf9cd2691a3f1
Hong <dbsghdz1@gmail.com>
2025-07-24 17:14:48 +0900

    feat: SFSymbol 아이콘 지정

---
+++ HongAndJerry/HongAndJerry/Source/DesignSystem/Icon/Icon.swift
@@ -0,0 +1,17 @@
+//
+//  Icon.swift
+//  HongAndJerry
+//
+//  Created by Hong on 7/24/25.
+//
+
+enum Icon {}
+
+extension Icon {
+    enum SFsymbol {
+        static let chevronLeft = "chevron.left"
+        static let playFill = "play.fill"
+        static let pauseFill = "pause.fill"
+        static let arrowUpDown = "arrow.up.left.and.arrow.down.right"
+    }
+}
