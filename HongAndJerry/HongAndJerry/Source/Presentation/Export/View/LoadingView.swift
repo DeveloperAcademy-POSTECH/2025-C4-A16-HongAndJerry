4da9427f39d233a1e7380296269cd238196d333e
Hong <dbsghdz1@gmail.com>
2025-07-30 17:04:41 +0900

    feat: Lottie 파일추가

---
+++ HongAndJerry/HongAndJerry/Source/Presentation/Export/View/LoadingView.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingVIew.swift
+//  HongAndJerry
+//
+//  Created by Hong on 7/30/25.
+//
+
+import SwiftUI
+
+struct LoadingView {
+    
+}
+
+extension LoadingView: View {
+    var body: some View {
+        
+    }
+}
---
+++ HongAndJerry/HongAndJerry/Source/Presentation/Export/View/LottieView.swift
@@ -0,0 +1,7 @@
+//
+//  LottieView.swift
+//  HongAndJerry
+//
+//  Created by Hong on 7/30/25.
+//
+
