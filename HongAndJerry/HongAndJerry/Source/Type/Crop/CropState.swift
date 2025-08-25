cdf2d80469b15772389509f946ba84488451599a
ehdrbdndns <ehdrbdndns@naver.com>
2025-08-25 16:21:36 +0900

    refactor: 사용하지 않는 파일 삭제 및 폴더 구조 통일

---
+++ HongAndJerry/HongAndJerry/Source/Type/Crop/CropState.swift
@@ -0,0 +1,13 @@
+//
+//  CropState.swift
+//  HongAndJerry
+//
+//  Created by Donggyun Yang on 8/25/25.
+//
+
+enum CropState {
+    case thumbnailLoading
+    case thumbnailLoaded
+    case cropping
+    case completedConvertToAsset
+}
